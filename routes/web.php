1632e1d13603086dd359471e1682634af233d991
Unknown <faithmumbua74@gmail.com>
2018-07-05 09:18:27 +0300

    routes

--- routes/web.php
+++ routes/web.php
@@ -10,4 +10,19 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+//User
+Route::post('/register','User_94120Controller@register');
+Route::post('/login','User_94120Controller@login');
+Route::get('/showusers', 'User_94120Controller@showUsers');
 
+//Session 
+Route::post('/save_session','SessionController@save');
+Route::get('/show_session','SessionController@session');
+
+//Instructors
+Route::post('/saveInstructor','instructorController@saveInstructor');
+Route::get('/showinstructors','instructorController@showinstructors');
+
+//Gym
+Route::post('/saveGym','gym_locationController@saveGym');
+Route::get('/showGym','gym_locationController@showGym');
