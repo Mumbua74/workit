f6e57736483f1f25b6e73ce595c55a15cc231049
Unknown <faithmumbua74@gmail.com>
2018-07-05 09:27:23 +0300

    slash

--- routes/web.php
+++ routes/web.php
@@ -11,18 +11,18 @@
 |
 */
 //User
-Route::post('/register','User_94120Controller@register');
-Route::post('/login','User_94120Controller@login');
+Route::post('register','User_94120Controller@register');
+Route::post('login','User_94120Controller@login');
 Route::get('/showusers', 'User_94120Controller@showUsers');
 
 //Session 
-Route::post('/save_session','SessionController@save');
+Route::post('save_session','SessionController@save');
 Route::get('/show_session','SessionController@session');
 
 //Instructors
-Route::post('/saveInstructor','instructorController@saveInstructor');
+Route::post('saveInstructor','instructorController@saveInstructor');
 Route::get('/showinstructors','instructorController@showinstructors');
 
 //Gym
-Route::post('/saveGym','gym_locationController@saveGym');
+Route::post('saveGym','gym_locationController@saveGym');
 Route::get('/showGym','gym_locationController@showGym');
