3ca7a4d102582343b96bcef9ce6647940e515256
Unknown <faithmumbua74@gmail.com>
2018-07-05 10:21:10 +0300

    Disabled CSRF for all routes

--- routes/api.php
+++ routes/api.php
@@ -14,22 +14,7 @@ use Illuminate\Http\Request;
 */
 
 
-//User
-Route::post('/register','User_94120Controller@register');
-Route::post('/login','User_94120Controller@login');
-Route::get('/showusers', 'User_94120Controller@showUsers');
 
-//Session 
-Route::post('/save_session','SessionController@save');
-Route::get('/show_session','SessionController@session');
-
-//Instructors
-Route::post('/saveInstructor','instructorController@saveInstructor');
-Route::get('/showinstructors','instructorController@showinstructors');
-
-//Gym
-Route::post('/saveGym','gym_locationController@saveGym');
-Route::get('/showGym','gym_locationController@showGym');
 
 
 //
--- routes/web.php
+++ routes/web.php
@@ -10,19 +10,21 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+
 //User
-Route::post('register','User_94120Controller@register');
-Route::post('login','User_94120Controller@login');
+Route::post('/register','User_94120Controller@register');
+Route::post('/login','User_94120Controller@login');
 Route::get('/showusers', 'User_94120Controller@showUsers');
 
 //Session 
-Route::post('save_session','SessionController@save');
+Route::post('/save_session','SessionController@save');
 Route::get('/show_session','SessionController@session');
 
 //Instructors
-Route::post('saveInstructor','instructorController@saveInstructor');
+Route::post('/saveInstructor','instructorController@saveInstructor');
 Route::get('/showinstructors','instructorController@showinstructors');
 
 //Gym
-Route::post('saveGym','gym_locationController@saveGym');
+Route::post('/isaveGym','gym_locationController@saveGym');
 Route::get('/showGym','gym_locationController@showGym');
